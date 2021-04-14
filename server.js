a2946c006c26fdf1eff30444d86e9869ccfa1073
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-14 10:23:05 +0200

    added endpoints for login

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.post('/api/register', (req, res) => {});
+app.post("/api/login", (req, res) => {});
+app.get("/api/users", (req, res) => {});
+app.delete("/api/logout", (req, res) => {});
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localHost:${port}`)
